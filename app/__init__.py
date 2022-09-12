ecd48df605219497155064a9780fa418739203bd
Iho Somnam <ihosomnam4741@gmail.com>
2022-09-13 01:03:56 +0700

    add more,....

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,65 @@
-from flask import Flask, jsonify, request
-from flask_sqlalchemy import SQLAlchemy
+import json
+
+from flask import Flask, jsonify, request, Response, render_template
+from flask_wtf.csrf import CSRFProtect
+from influxdb_client import InfluxDBClient
+from app.influx import token, org, bucket
+from influxdb_client.client.write_api import SYNCHRONOUS
+
 
 # Initialize
-
 app = Flask(__name__)
-db = SQLAlchemy(app)
+# csrf = CSRFProtect()
+# csrf.init_app(app)
 
 # Config
-
 app.config['DEBUG_MODE'] = True
-app.config['SECRET_KEY'] = 'E09D9689D8511DA1D1575720FF7A2B99CC1494BA'
-app.config['SQLALCHEMY_BINDS'] = {'local': 'sqlite:///database.db'}
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SECRET_KEY'] = 'TheCureliestWalk'
+
 
 # Routes
-
 @app.route('/', methods=['GET', 'POST'])
 def home():  # put application's code here
-    if request.method == 'GET':
-        data = {'message': 'App is running...'}
-    else:
-        data = {'message':'Not valid method.'}
-    return jsonify(data)
+    return render_template('index.html')
+
+
+@app.route('/get', methods=['GET'])
+def getData():
+    data = []
+    with InfluxDBClient(url="http://10.101.118.91:8086", token=token, org=org) as client:
+        query = 'from(bucket: "{}") |> range(start: -1h)'.format(bucket)
+        tables = client.query_api().query(query, org=org)
+        for table in tables:
+            for i, record in enumerate(table.records):
+                # Print on console
+                # print(record["_measurement"], record["_field"], record["_value"])
+                # measurement.append(record["_measurement"])
+                # field.append(record["_field"])
+                # value.append(record["_value"])
+                data.append({ "id": i, "_measurement": record["_measurement"], "_field": record["_field"], '_value': record["_value"], "timestamp": record["_time"]})
+        client.close()
+        return jsonify(data)
+
+
+@app.route('/<measurement>/<data_key>/<data_value>/<field_key>/<field_value>', methods=['POST'])
+def writeData(measurement, data_key, data_value, field_key, field_value):
+    # measurement = "flask_app"
+    # data_key = "location"
+    # data_value = 1
+    # field_key = "last_speed_record"
+    # field_value = 48
+
+    measurement = request.view_args['measurement']
+    data_key = request.view_args['data_key']
+    data_value = request.view_args['data_value']
+    field_key = request.view_args['field_key']
+    field_value = request.view_args['field_value']
+
+    with InfluxDBClient(url="http://10.101.118.91:8086", token=token, org=org) as client:
+        write_api = client.write_api(write_options=SYNCHRONOUS)
+        # "measurement,data _field"
+        data = f"{measurement},{data_key}={data_value} {field_key}={field_value}"
+        # -------------------------^------------- index of location 1: ex. "Kang Sa Dan"
+        write_api.write(bucket, org, data)
+        client.close()
+    return jsonify({"message": "Data write successfully!"}), 200
--- app/influx.py
+++ app/influx.py
@@ -1,34 +1,3 @@
-from datetime import datetime
-from influxdb_client import InfluxDBClient
-from influxdb_client.client.write_api import SYNCHRONOUS
-
-class InfluxUp():
-    def __init__(self):
-        self.token = "u4PvOC7Pm0wCHZSy2q-yZgRTkRN1OY-p42OTKQeAgw0tmjs29cjCrDEuzSwafE8-TLEOIdcgKkTJR873kdiHzA=="
-        self.org = "kkuiot"
-        self.bucket = "iho"
-        # Data
-        self.measurement = "flask_app"
-        self.data_key = "location"
-        self.data_value = 1
-        self.field_key = "last_speed_record"
-        self.field_value = 48
-
-        with InfluxDBClient(url="http://10.101.118.91:8086", token=self.token, org=self.org) as self.client:
-            self.write_api = self.client.write_api(write_options=SYNCHRONOUS)
-            # "measurement,data _field"
-            self.data = f"{self.measurement},{self.data_key}={self.data_value} {self.field_key}={self.field_value}"
-            # -------------------------^------------- index of location 1: ex. "Kang Sa Dan"
-            self.write_api.write(self.bucket, self.org, self.data)
-
-            # Query
-
-            self.query = 'from(bucket: "iho") |> range(start: -1m)'
-            self.tables = self.client.query_api().query(self.query, org=self.org)
-            for table in self.tables:
-                for record in table.records:
-                    print(record["_measurement"], record["_field"], record["_value"])
-            self.client.close()
-
-# calling for class
-init = InfluxUp()
+token = "u4PvOC7Pm0wCHZSy2q-yZgRTkRN1OY-p42OTKQeAgw0tmjs29cjCrDEuzSwafE8-TLEOIdcgKkTJR873kdiHzA=="
+org = "kkuiot"
+bucket = "iho"
